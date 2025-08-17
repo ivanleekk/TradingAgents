6418d1a906f15ca5f96eaa7c07c795766a17f8ee
Ivan Lee <84584280+ivanleekk@users.noreply.github.com>
2025-08-17 21:18:30 +0800

    fix: undo change and add 2 gpu

--- tradingagents/graph/trading_graph.py
+++ tradingagents/graph/trading_graph.py
@@ -96,7 +96,11 @@ class TradingAgentsGraph:
                 trust_remote_code=True,
                 tensor_parallel_size=1,
             )
-            self.quick_thinking_llm = self.deep_thinking_llm
+            self.quick_thinking_llm = VLLM(
+                model=self.config["quick_think_llm"],
+                trust_remote_code=True,
+                tensor_parallel_size=1,
+            )
         else:
             raise ValueError(f"Unsupported LLM provider: {self.config['llm_provider']}")
 
