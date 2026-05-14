b2c8230ad0ce7c8ba7e825573c21ab057ec1c23b
Ivan Lee <84584280+ivanleekk@users.noreply.github.com>
2026-05-14 18:35:43 +0800

    feat: ablation

--- submit_ablation.sh
+++ submit_ablation.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=32G
+#SBATCH --mem=64G
 #SBATCH --time=72:00:00
 
 # Create logs directory if it doesn't exist
